e4a5b5c72e54c51ef29aabbba80c0387e6075b08
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-12-19 11:43:10 -0300

    exer.01

---
+++ HellowWorld/HellowWorld/Program.cs
@@ -0,0 +1,46 @@
+﻿using System; //manipular coisas do sistema operacional do usuario
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks; Apenas se for usar varios nucleos do processador
+//using System.IO; Para manipular pastas 
+
+namespace HellowWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int - 123
+            //Float - 12.6
+            //Bool - true ou false
+            //String - abcd
+            //Char - 'a' caracter
+
+            String nome = "";
+
+            Console.WriteLine("Seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: " + nome);
+               
+
+            float velocidadeF1 = 294.6f; //Float vai f no final
+            bool verdadeiroOuFalso = true;
+
+            //dynamic pode mudar a tipagem da variavel
+            dynamic cor_favorita = "Vermelho";
+            Console.WriteLine("");
+            Console.WriteLine(cor_favorita);
+            cor_favorita = 123;
+            Console.WriteLine("Passando para int:" + cor_favorita);
+
+            //Divisão utilizar float 
+            //float div = 5.0f/2;
+            //Console.WriteLine(div);
+
+
+            Console.ReadLine();//Espera o Enter
+        }
+    }
+}
---
+++ exercicio01/exercicio01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+//Pegue uma idade e informe se é uma criança, adolecente, adulto ou idoso. 
+//ultilizando laço de repetição, perguntar se gostaria de repetir a pergunta.
+
+namespace exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char op;
+
+            do
+            {
+                Console.WriteLine("Informe sua idade:");
+                int idade = int.Parse(Console.ReadLine());
+
+                if (idade >= 0 && idade <= 11)
+                {
+                    Console.WriteLine("Criança: " + idade);
+                }
+                else if (idade <= 18)
+                {
+                    Console.WriteLine("Adolecente: " + idade);
+                }
+                else if (idade <= 60)
+                {
+                    Console.WriteLine("Adulto: " + idade);
+                }
+                else
+                {
+                    Console.WriteLine("Idoso: " + idade);
+                }
+
+                Console.WriteLine("\nRepetir? 'S' \n Não Repetir? 'N ");
+                op = char.Parse(Console.ReadLine());
+
+
+            } while (op == 'S' || op == 's');
+            
+        }
+    }
+}
