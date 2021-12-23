c4098991a3ac3e287de847cfa7f7aabcf0c7a1d1
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-12-22 23:34:36 -0300

    FormatandoSaidaNoConsole

---
+++ FormatandoSaidaNoConsole/FormatandoSaidaNoConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace FormatandoSaidaNoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorCompra = 5.50;
+            double valorVenda;
+            double lucro = 0.1;
+            string produto = "Pastel";
+
+            valorVenda = valorCompra + (valorCompra*lucro);
+
+            //{indice: 0, valor: 0.15, formatoReal: c  formatoPorcentagem: p}
+            Console.WriteLine("Produto:.....:{0,15}", produto);
+            Console.WriteLine("Val.Compra:..:{0,15:c}", valorCompra);
+            Console.WriteLine("Lucro:.......:{0,15:p}", lucro);
+            Console.WriteLine("Vl.Venda:....:{0,15:c}", valorVenda);
+
+            Console.ReadLine();
+        }
+    }
+}
