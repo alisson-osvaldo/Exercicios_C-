8f522c1104ae8d19af18e82fc0a44a0bb24d1fdd
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-12-24 19:04:35 -0300

    switch_goto_enum_typecast

---
+++ Conversoes_De_Tipos(typecast)/Conversoes_De_Tipos(typecast)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Conversoes_De_Tipos_typecast_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Conversão implícita, é feito altomáticamente
+            int m1 = 10;
+            float m2 = m1;
+
+            //Conversão explicita, é feito manualmente
+            float n1 = 10.5f;
+            int n2 = (int)n1; //type cast
+            Console.WriteLine(n2);
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
---
+++ Enumeradores(enum)/Enumeradores(enum)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Enumeradores_enum_
+{
+    internal class Program
+    {
+        //Enumeradores nos permite criar um tipo próprio com valores prédefinidos,
+        //e esses valores poder ser associados ao indice que é a sua posição dentro do conjunto de valores{} do enumerador.
+        enum DiasSemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };
+        static void Main()
+        {
+
+            DiasSemana ds = (DiasSemana)3; //indice 3 = Quarta
+
+            //pegando o valor do indice
+            int indice = (int)DiasSemana.Quarta;
+
+            Console.WriteLine(ds);
+            Console.WriteLine("Valor do indice:" + indice);
+            
+            Console.ReadLine();
+        }
+    }
+}
