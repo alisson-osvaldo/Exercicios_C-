eb46c40aa8b00e6276ddd294df3a5020d58d3c19
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-12-24 18:58:39 -0300

    switch/goto/enum/typecast

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main() 
+        {
+            char escolha;
+            int tempo = 0;
+
+            inicio://goto: para onde vamos voltar
+
+            Console.Clear();    
+
+            Console.WriteLine("Viagem de:");
+            Console.WriteLine("Belo Horizonte/MG para Curitiba/PR");
+            Console.WriteLine("Escolha o tranporte: [a]Avião  -  [b]Carro  -  [c]}ônibus");
+            escolha = char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 'a':
+                case 'A':
+                    tempo = 50;
+                    break;
+                case 'b':
+                case 'B':
+                    tempo = 480;
+                    break;
+                case 'c':
+                case 'C':
+                    tempo = 660;
+                    break;
+
+                default://caso nenhuma das opções 
+                    tempo =-1;
+                    break;
+            }
+
+            if (tempo < 0)
+            {
+                Console.WriteLine("Tranporte indisponível");  
+            } else
+            {
+                Console.WriteLine("Tempo {0} minutos", tempo);
+            }
+
+            Console.Write("\nEscolher outro transporte? SIM='S' Não='N'");
+            escolha = char.Parse(Console.ReadLine());
+            if (escolha == 'S' || escolha == 's')
+            {
+                goto inicio;
+            } else
+            {
+                Console.Clear();
+                Console.WriteLine("Fim do programa!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
