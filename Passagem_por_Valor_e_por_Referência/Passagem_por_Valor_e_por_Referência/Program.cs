c924d88945fb1bdf52b11505c665ff6cf5e0fa3b
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-12-26 15:31:17 -0300

    passagem por valor e por referência

---
+++ Passagem_por_Valor_e_por_Referência/Passagem_por_Valor_e_por_Referência/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagem_por_Valor_e_por_Referência
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = 10;
+            int n   = 10;
+            dobrar1(num);
+            Console.WriteLine("Por Valor: " + num);
+            dobrar2(ref n);
+            Console.WriteLine("\nPor Referência: " + n);
+
+            Console.ReadLine();
+        }
+        //passagem por valor, cria um novo endereço na memória, num continuaria valendo 10 e valor seria uma nova variavel na memória com valor 20
+        static void dobrar1(int valor)
+        {
+            valor *= 2;
+        }
+        //passagem por referência, usa o endereço de memória da variavel que foi passada como argumento 
+        static void dobrar2(ref int valor)
+        {
+            valor *= 2;
+        }
+    }
+}
