fc696780b0311c8c195d5fb36df511757a275f97
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-12-31 14:13:15 -0300

    Variável constante

---
+++ Variavel_Constante/Variavel_Constante/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variavel_Constante
+{
+    internal class Program
+    {
+        //Variavel Constante
+        public const int SAIDA_PROGRAMA = 1;
+
+        static void Main()
+        {
+            while (true) //Se for verdadeiro
+            {
+                Console.WriteLine("\nDigite " + SAIDA_PROGRAMA + " para sair do programa:");
+                int valor = int.Parse(Console.ReadLine());//recebendo do telcado + convertendo string para inteiro
+
+                if (SAIDA_PROGRAMA == valor)
+                {
+                    break;//pausa
+                }
+            }           
+        } 
+    }
+}
